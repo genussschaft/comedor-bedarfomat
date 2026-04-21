cc6226c704285b4f092215f39bb575131a46c51d
naokipeter <naoki.l.peter@gmail.com>
2026-04-21 12:04:08 +0200

    Add deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/comedor-bedarfomat/',
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
 })
